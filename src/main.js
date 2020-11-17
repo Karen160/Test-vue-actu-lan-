756ff186e8aa82002ee25bf3c7f58c26ec98f095
Karen <karen.azoulay@edu.devinci.fr>
2020-11-17 14:22:07 +0100

    est

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import Tab from './home.vue'
+import router from './router'
 
-createApp(App).mount('#app')
-createApp(Tab).mount('#tab')
+createApp(App).use(router).mount('#app')
+createApp(Tab).use(router).mount('#tab')
