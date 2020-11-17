0800d5c4deb889080a06da252aad742915f19bbe
Karen <karen.azoulay@edu.devinci.fr>
2020-11-17 11:56:36 +0100

    test

--- src/App.vue
+++ src/App.vue
@@ -6,22 +6,12 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue';
-import Home from './components/Home.vue';
-
 
 export default {
-<<<<<<< HEAD
-    name: 'App',
-    components: {
-        HelloWorld,
-    }
-=======
   name: 'App',
   components: {
     HelloWorld,
-    Home,
   }
->>>>>>> 9a38caa225c3b22ea7962a71a58df0bc3114c9a3
 }
 </script>
 
