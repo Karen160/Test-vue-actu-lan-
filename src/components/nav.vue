98280dbceffd8566c19aa96de081b7250f5f7953
Karen <karen.azoulay@edu.devinci.fr>
2020-11-20 17:14:57 +0100

    nav

---
+++ src/components/nav.vue
@@ -0,0 +1,35 @@
+<template>
+    <nav>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+        <li>Share</li>
+    </nav>
+</template>
+
+<script>
+export default {
+  name: 'navTest',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
