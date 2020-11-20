09ca2b3712966127e52ec58c73f8663c61d6de9a
= <=>
2020-11-20 14:05:03 +0100

    reset

--- src/main.js
+++ src/main.js
@@ -1,12 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import Tab from './home.vue'
-import router from './router'
 
-Vue.config.productionTip = false
-
-new Vue({
-	router,
-	render: (h) => h(App)
-}).$mount('#app')
-
+createApp(App).mount('#app')
