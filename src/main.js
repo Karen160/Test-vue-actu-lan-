3ac101912552d66ba78b7ef24ffa052f38f1db19
= <=>
2020-11-20 14:50:56 +0100

    fix

--- src/main.js
+++ src/main.js
@@ -1,12 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-<<<<<<< HEAD
-import Tab from './home.vue'
 import router from './router'
 
 createApp(App).use(router).mount('#app')
-createApp(Tab).use(router).mount('#tab')
-=======
 
-createApp(App).mount('#app')
->>>>>>> 6d8d1b4a861979fd6e31c730472c4f4a21bf2a34
