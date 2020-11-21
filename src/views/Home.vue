4cbd4f8d481e8bd32422f25c69d9a5f0a72cb6ff
Nathan Szigeti <nathan.szigeti@edu.devinci.fr>
2020-11-21 15:07:19 +0100

    reglage bug

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,22 +4,21 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
-  <Gmap></Gmap>
   <foooter></foooter>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-import foooter from '@/components/footer.vue'
-import Gmap from '@/components/map.vue'
+ import foooter from '@/components/footer.vue'
 
 
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    foooter
   }
 }
 </script>
